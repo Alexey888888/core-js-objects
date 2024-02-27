3fee4fbbb573e0656fd327976e9c2a31d3cbd225
Alexey888888 <brdkmail@gmail.com>
2024-02-27 15:31:29 +0200

    feat: implement fromJSON function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -212,8 +212,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const resObj = JSON.parse(json);
+  return Object.setPrototypeOf(resObj, proto);
 }
 
 /**
